64ed5293da928dc970efee0d79b73c4d60250b52
yu <araera111@gmail.com>
2022-09-03 18:52:04 +0900

    feat:

---
+++ src/utils/util.spec.ts
@@ -0,0 +1,13 @@
+import { add } from './util';
+
+describe('add', () => {
+	it('case1: 1 + 2 = 3', () => {
+		expect(add(1, 2)).toBe(3);
+	});
+});
+
+describe('splitMessage', () => {
+	it('', () => {
+		expect().toBe();
+	});
+});
