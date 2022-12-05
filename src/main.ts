cfa7e606bd9ea0c7091ed676d0c5d24e8badbe34
yuumillar <araera111@gmail.com>
2022-12-05 23:11:16 +0900

    feat:

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 type User = {
   id: number;
   name: string;
@@ -7,5 +9,11 @@ const basicUser: User = {
   id: 0,
   name: ''
 };
-const main = async () => {};
+const main = async () => {
+  const user2 = produce(basicUser, (draft) => {
+    draft.id = 1;
+    draft.name = 'tanaka';
+  });
+  console.log(user2);
+};
 main();
--- src/utils/util.spec.ts
+++ src/utils/util.spec.ts
@@ -1,7 +1,7 @@
 import { add } from './util';
 
 describe('add', () => {
-	it('case1: 1 + 2 = 3', () => {
-		expect(add(1, 2)).toBe(3);
-	});
+  it('case1: 1 + 2 = 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
 });
