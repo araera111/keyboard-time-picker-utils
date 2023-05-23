f7f1fe7030b3562cd794e9a8823cfd78624cf3cd
yuumillar <araera111@gmail.com>
2023-05-23 22:42:43 +0900

    feat:

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,3 @@
-import cors from 'cors';
-import express, { json } from 'express';
-import pino from 'pino-http';
+const main = () => {};
 
-const logger = pino();
-
-const app = express();
-const port = 22004;
-app.use(cors());
-app.use(json());
-app.use(logger);
-
-app.get('/', (req, res) => {
-  res.send('HELLO WORLD NODE SERVER...2');
-});
-
-app.listen(port, () => {
-  console.log(`start server http://127.0.0.1:22001`);
-});
+main();
