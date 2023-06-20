bfb7ade30759782413cf97011945c316a502b2b9
yuumillar <araera111@gmail.com>
2023-06-20 12:22:25 +0900

    feat:

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
-
-const app = new Hono();
-app.get("/", (c) => c.text("Hello Node.js!"));
-
-serve(app);
+const main = () => {
+  console.log("main");
+};
+main();
