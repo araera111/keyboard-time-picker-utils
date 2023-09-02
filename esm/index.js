792e591b6f44fc328f55d616c670e9131ba12e39
Ou Fumio <63596736+araera111@users.noreply.github.com>
2023-09-02 17:11:40 +0900

    feat:

---
+++ esm/index.js
@@ -0,0 +1,8 @@
+// Bundled with Packemon: https://packemon.dev
+// Platform: browser, Support: stable, Format: esm
+
+const main = () => {
+  console.log("main");
+};
+main();
+//# sourceMappingURL=index.js.map
